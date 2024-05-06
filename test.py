9b6dab305a5aedbde4c8f8ec23132cd66509811b
anhnguyen06 <147959102+anhnguyen06@users.noreply.github.com>
2024-05-07 00:31:47 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
 import math as math
 import time
 st.write("hello word")
