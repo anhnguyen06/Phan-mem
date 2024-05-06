79475b075aaa7996ae983547deedeb5ed1b60d1c
anhnguyen06 <147959102+anhnguyen06@users.noreply.github.com>
2024-05-07 00:32:34 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import pandas as pd
 import math as math
+from ipywidgets import interact
+import ipywidgets as ipw
+
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+from bokeh.io import show, output_notebook, push_notebook
+
+from tempfile import mkdtemp
+import os  # Thêm dòng này để import module os
+
 import time
 st.write("hello word")
