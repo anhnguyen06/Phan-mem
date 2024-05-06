a3f8fc2ba28553f45da66c5418bc3e90b980b247
anhnguyen06 <147959102+anhnguyen06@users.noreply.github.com>
2024-05-07 00:30:34 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,5 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 import math as math
-
-
 import time
-
-output_notebook()
 st.write("hello word")
