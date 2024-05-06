157b984ac226f5ad3d946d90eed79db117df0918
anhnguyen06 <147959102+anhnguyen06@users.noreply.github.com>
2024-05-07 00:28:30 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,7 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 import math as math
-from ipywidgets import interact
-import ipywidgets as ipw
 
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource
-from bokeh.io import show, output_notebook, push_notebook
 
 import time
 
